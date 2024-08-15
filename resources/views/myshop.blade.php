74009cea855509542bad3efdc76b819ddb5a26a1
txng222 <watcharakorn.j@kkumail.com>
2024-08-15 17:38:41 +0700

    first commit

---
+++ resources/views/myshop.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>เกี่ยวกับร้านIT Shop</h1>
+    <ul>
+        <li>ที่อยู่ร้าน : {{$address}} </li>
+        <li>เบอร์โทร : {{$mobilephone}} </li>
+        <li>ที่ตั้งร้าน : {{$location}} </li>
+    </ul>
+</body>
+</html>
